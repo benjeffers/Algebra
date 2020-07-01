70d64b7fe30f7d956aa4f9b48cdab5c05385b55c
Benjamin Jeffers <45741895+benjeffers@users.noreply.github.com>
2020-07-01 18:03:01 -0500

    more finite group methods

--- TestGroup.py
+++ TestGroup.py
@@ -103,10 +103,88 @@ class TestGroup(unittest.TestCase):
         G = self.create_group_additive()
         H_1 = Group([0], lambda x, y: (x+y)%4)
         H_2 = Group([0, 2], lambda x, y: (x+y)%4)
-        subgroups = [H_1, H_2]
-        for g in G.subgroups():
-            print(g.set)
-        self.assertTrue(G.subgroups() == subgroups)
+        subgroups = [H_1.set, H_2.set]
+        G_subgroup_sets = []
+        for group in G.subgroups():
+            G_subgroup_sets.append(group.set)
+        self.assertTrue(G_subgroup_sets == subgroups)
+
+
+    def test_left_coset(self):
+        G = self.create_group_additive()
+        H = Group([0, 2],  G.func)
+        coset = G.left_coset(H, 1)
+        self.assertTrue(coset == Group([1, 3], G.func))
+
+    def test_right_coset(self):
+        G = self.create_group_additive()
+        H = Group([0, 2],  G.func)
+        coset = G.right_coset(H, 1)
+        self.assertTrue(coset == Group([1, 3], G.func))
+
+
+    def test_equality(self):
+        G = Group([1, 2, 3, 4, 5], lambda x, y: (x + y)%5)
+        H = Group([1, 2, 3, 4, 5], lambda x, y: (x + y)%5)
+        self.assertTrue(G == H)
+
+    def test_equality_false(self):
+        G = self.create_group_additive()
+        H = self.create_group_multiplicative()
+        self.assertFalse(G == H)
+
+    def test_inequality(self):
+        G = Group([1, 2, 3, 4, 5], lambda x, y: (x + y)%5)
+        H = Group([1, 2, 3, 4], lambda x, y: (x + y)%4)
+        self.assertTrue(G != H)
+
+    def test_inequality_false(self):
+        G = Group([1, 2, 3, 4, 5], lambda x, y: (x + y)%5)
+        H = Group([1, 2, 3, 4, 5], lambda x, y: (x + y)%5)
+        self.assertFalse(G != H)
+
+    def test_is_abelian(self):
+        G = self.create_group_additive()
+        self.assertTrue(G.is_abelian())
+
+    def test_normal(self):
+        G = self.create_group_additive()
+        H = Group([0, 2], G.func)
+        self.assertTrue(G.is_normal(H))
+
+    def test_order_identity(self):
+        G = self.create_group_additive()
+        self.assertTrue(G.order(0) == 1)
+
+    def test_order_one(self):
+        G = self.create_group_additive()
+        self.assertTrue(G.order(1) == 4)
+
+    def test_order_two(self):
+        G = self.create_group_additive()
+        self.assertTrue(G.order(2) == 2)
+
+    def test_order_three(self):
+        G = self.create_group_additive()
+        self.assertTrue(G.order(3) == 4)
+    
+    def test_group_generated_by(self):
+        G = self.create_group_additive()
+        H = Group([0, 2], G.func)
+        self.assertTrue(G.group_generated_by(2) == H)
+
+    def test_group_generated_by_cyclic(self):
+        G = self.create_group_additive()
+        self.assertTrue(G.group_generated_by(3) == G)
+
+    def test_is_cylic(self):
+        G = self.create_group_additive()
+        self.assertTrue(G.is_cylic())
+
+    def test_is_cylic_false(self):
+        G = self.create_group_multiplicative()
+        self.assertFalse(G.is_cylic())
+        
 
 if __name__ == '__main__':
     unittest.main()
